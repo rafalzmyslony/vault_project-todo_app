1805c3f3d258106a2c1a132e0aa69bb7318b0756
rafalzmyslony <33902552+rafalzmyslony@users.noreply.github.com>
2023-07-27 13:23:59 +0200

    Update get_password_from_vault.py v3

--- get_password_from_vault.py
+++ get_password_from_vault.py
@@ -6,6 +6,7 @@ import os
 Script prints password from HCP Vault kept in this path: kv/data/db/todo_app from field password (kv/data/db/todo_app/password
 
 '''
+_todo_env_location = 'todo_env'
 _VAULT_ADDRESS = os.getenv('VAULT_ADDR')
 
 # get credentials from EC2 metadata (vault is auth via IAM role)
@@ -23,5 +24,16 @@ read_secret_result = client.secrets.kv.v2.read_secret(
     path=secret_path,
     mount_point=mount_point
 )
+password = read_secret_result['data']['password']
 
-print(read_secret_result['data']['password'])                                                       
+# replace password in todo_env file
+with open('_todo_env_location', "r") as f1:
+    lines = f1.readlines()
+
+for i, line in enumerate(lines):
+    if 'DB_PASSWORD' in line:
+        lines[i] = 'DB_PASSWORD='+format(password)
+        with open(todo_env_location, "w") as f:
+            f.writelines(lines)
+        f.close()
+f1.close()
